09b76e6d6a8e0aba541c94cd64d79bbf874c94bd
Rene Sanchez <renesanchez@yahoo.com>
2017-03-31 08:14:07 -0500

    completed

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ $(function(){
 
           $('.icon').html('<img src="http://openweathermap.org/img/w/' + Icon + '.png">');
           $('.flag').html('<img src="https://lipis.github.io/flag-icon-css/flags/4x3/' + flag + '.svg">');
-          $('.map').html('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBYVFaVlAm7z6PBipNIaVb-6SZxWY8Cj-o&q=' + city + '" allowfullscreen></iframe>');
+          $('.map').html('<iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/search?key=AIzaSyBYVFaVlAm7z6PBipNIaVb-6SZxWY8Cj-o&q=' + city + flag + '" allowfullscreen></iframe>');
     });
 
 
